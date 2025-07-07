7c4dd12b3743c499cb1b45b56e2a90c41822b44a
Shashwat Bagaria <shashwawtforwork@gmail.com>
2025-07-07 08:52:30 +0530

    BE 1

---
+++ 5-BI-1/index.js
@@ -0,0 +1,22 @@
+// console.log(global);
+// console.log(__dirname, __filename);
+
+const fs = require("fs");
+const path = require("path");
+
+// const http = require("http");
+// const server = http.createServer();
+
+// const content = Math.random().toString(36).repeat(1000000);
+// fs.writeFileSync(path.join(__dirname, "big-file.txt"), content);
+
+// server.on("request", (req, res) => {
+//   fs.readFile("./big-file.txt", (err, data) => {
+//     if (err) throw err;
+//     res.end(data);
+//   });
+// });
+
+// server.listen(3000, () => {
+//   console.log("Server started at 3000");
+// });
